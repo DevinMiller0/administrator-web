6ebb8074b10d61c58ea36d8d98ea5f3d0e1b2b75
wangdesheng <wangdesheng2017@gmail.com>
2020-06-03 23:30:43 +0800

    .

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,19 +3,17 @@
     <el-card class="box-card">
       <h2 class="text-login">LOGIN</h2>
 
-      <el-form ref="form" label-width="80px" class="loginForm">
-
-        <el-form-item prop="name">
-          <el-input type="text" placeholder="请输入用户名"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input type="password" auto-complete="off" placeholder="请输入密码"></el-input>
+      <el-form ref="form" class="login-form">
+        <el-form-item>
+          <el-input v-model="username" type="text" placeholder="请输入用户名"/>
         </el-form-item>
         <el-form-item>
-          <el-button class="homeBut" type="primary">登录</el-button>
+          <el-input type="password" placeholder="请输入密码"/>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn-login" type="primary">登录</el-button>
         </el-form-item>
       </el-form>
-
     </el-card>
 
   </div>
@@ -24,7 +22,10 @@
 
 <script>
   export default {
-    name: "login"
+    data: {
+      username: '',
+      password: '',
+    }
   }
 </script>
 
@@ -39,7 +40,7 @@
 
   .box-card {
     width: 480px;
-    height: 400px;
+    height: 360px;
     text-align: center;
   }
 
@@ -47,8 +48,12 @@
     line-height: 60px;
   }
 
-  .el-form-item {
-    margin-left: 0;
+  .login-form {
+    margin-left: 50px;
+    margin-right: 50px;
   }
 
+  .btn-login {
+    width: 120px;
+  }
 </style>
